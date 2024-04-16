d137adcf5cb98c66a1e1bebe0711c60a7f634b5b
casey <caseymwangi31@gmail.com>
2024-04-16 11:34:48 +0300

    finished the ist challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('%c HI', 'color: firebrick');
+
+
+    //challenge 1: fetch images and render them to the DOM
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+      .then(response => response.json())
+      .then(data => {
+        data.message.forEach(imageUrl => {
+            const img =document.createElement("img");
+            img.src = imageUrl;
+            document.getElementById("dog-image-container").appendChild(img);
+        })
+      })
+      .catch(error => console.error("There was an error!!", error));
+})
+
